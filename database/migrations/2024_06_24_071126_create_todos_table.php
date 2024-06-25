9cbb767109ed1a614ff21da7d7b0d8dde292f590
muazkhairi92 <muazkhairi@alumni.iastate.edu>
2024-06-25 09:06:35 +0800

    refacor migration

--- database/migrations/2024_06_24_071126_create_todos_table.php
+++ database/migrations/2024_06_24_071126_create_todos_table.php
@@ -13,7 +13,7 @@ return new class() extends Migration {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
             $table->timestamps();
         });
     }
