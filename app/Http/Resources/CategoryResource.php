c2206210f2d129b976c491fa12c06c7331e635d3
muazkhairi92 <muazkhairi@alumni.iastate.edu>
2024-06-25 09:07:34 +0800

    update resource

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class TodoResource extends JsonResource
+class CategoryResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,12 +14,6 @@ class TodoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-         return [
-            'id' => $this->id,
-            'todo' => $this->name,
-            'category' => optional($this->category)->name,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-        ];
+        return parent::toArray($request);
     }
 }
