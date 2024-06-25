1901638dafede1f1b0a410a6a9d7ab1b2157c1db
muazkhairi92 <muazkhairi@alumni.iastate.edu>
2024-06-25 09:56:02 +0800

    adjust factories

--- database/factories/AllowedIpFactory.php
+++ database/factories/AllowedIpFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AllowedIp>
  */
 class AllowedIpFactory extends Factory
 {
